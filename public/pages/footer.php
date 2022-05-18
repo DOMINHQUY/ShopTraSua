635f7dd5fd40326d186392d56bc315594c975349
DOMINHQUY <minhquydo18@gmail.com>
2022-05-18 09:16:22 +0700

    TaoGiaoDien

---
+++ public/pages/footer.php
@@ -0,0 +1,4 @@
+<div class="clear"></div>
+        <div class="footer">
+            <p class="footer_coppyright">Coppyright by Hao developer 2022</p>
+        </div>
---
+++ public/pages/sidebar/sidebar.php
@@ -0,0 +1,18 @@
+<div class="sidebar">
+            <ul class="list_sidebar">
+
+                <li><a href="index.php?quanly=menu&id=1">Trà sữa truyền thống</a></li>
+                <li><a href="index.php?quanly=menu&id=2">Trà sữa chocolate</a></li>
+                <li><a href="index.php?quanly=menu&id=3">Trà sữa matcha</a></li>
+                <li><a href="index.php?quanly=menu&id=4">Trà sữa Hong Kong</a></li>
+                <li><a href="index.php?quanly=menu&id=5">Sữa tươi trân châu đường đen</a></li>
+                <li><a href="index.php?quanly=menu&id=6">Trà đào</a></li>
+                <li><a href="index.php?quanly=menu&id=7">Trà đào xã tắc</a></li>
+                <li><a href="index.php?quanly=menu&id=8">Trà dâu</a></li>
+                <li><a href="index.php?quanly=menu&id=9">Trà ổi</a></li>
+                <li><a href="index.php?quanly=menu&id=10">Iced Apple Tea</a></li>
+                <li><a href="index.php?quanly=menu&id=11">Iced Lemon Tea</a></li>
+                <li><a href="index.php?quanly=menu&id=12">Ice Green Tea</a></li>
+                
+            </ul>
+</div>
